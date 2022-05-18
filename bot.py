b4c91804687ead1020c8f47f7f3b8cb84ba859af
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2022-05-18 19:27:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,23 +2,35 @@
 import os
 
 import discord
+import random
 
-TOKEN = 'token'
+TOKEN = 'OTc1ODIyNjQ3Mzg2NTg3MTY2.GecGQQ.EKhnHTPE6TIyy2OlLLRltzeeEcVt6QYUuwcKiU'
 #GUILD = 'Test Server'
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    @client.event
-    async def on_ready():
-        print(f'{client.user.name} has connected to Discord!')
+    print(f'{client.user.name} has connected to Discord!')
 
-    @client.event
-    async def on_member_join(member):
-        await member.create_dm()
-        await member.dm_channel.send(
-            f'Hi {member.name}, welcome to my Discord server!'
-        )
+@client.event
+async def on_message(message):
+    username = str(message.author).split('#')[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f'{username}: {user_message} ({channel})')
 
+    if message.author == client.user:
+        return
+
+    if user_message.lower() == 'hello':
+        await message.channel.send(f'Hello {username}!')
+        return
+    elif user_message.lower() == 'bye':
+        await message.channel.send(f'See you later {username}!')
+        return
+    elif user_message.lower() == '!random':
+        response = f'This is your random number: {random.randrange(1000000)}'
+        await message.channel.send(response)
+        return
 client.run(TOKEN)
