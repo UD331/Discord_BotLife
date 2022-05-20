6375951e302fa8e00bc993c511a0ebe13c23b56b
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2022-05-20 18:09:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,12 +14,6 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
-    url = 'https://animechan.vercel.app/api/random'
-    html = urllib.request.urlopen(url).read()
-    soup = BeautifulSoup(html, 'html.parser')
-    result = soup.get_text().split(':')
-    final = result[3].split('}')
-    print(final[0])
 
 @client.event
 async def on_message(message):
@@ -31,14 +25,12 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if user_message.lower() == 'hello':
-        await message.channel.send(f'Hello {username}!')
-        return
-    elif user_message.lower() == 'bye':
-        await message.channel.send(f'See you later {username}!')
-        return
-    elif user_message.lower() == '!random':
-        response = f'This is your random number: {random.randrange(1000000)}'
-        await message.channel.send(response)
+    if user_message.lower() == '!jojo':
+        url = 'https://animechan.vercel.app/api/random'
+        html = urllib.request.urlopen(url).read()
+        soup = BeautifulSoup(html, 'html.parser')
+        result = soup.get_text().split(':')
+        final = result[3].split('}')
+        await message.channel.send(final[0])
         return
 client.run(TOKEN)
