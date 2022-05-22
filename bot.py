39351f58e9767c07d75e1dda4d99f9f8691518c6
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2022-05-22 17:52:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,9 +11,11 @@ TOKEN = 'OTc1ODIyNjQ3Mzg2NTg3MTY2.GecGQQ.EKhnHTPE6TIyy2OlLLRltzeeEcVt6QYUuwcKiU'
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
+
 
 @client.event
 async def on_message(message):
@@ -21,6 +23,11 @@ async def on_message(message):
     user_message = str(message.content)
     channel = str(message.channel.name)
     print(f'{username}: {user_message} ({channel})')
+
+    url_x = 'https://animechan.vercel.app/api/available/anime'
+    html = urllib.request.urlopen(url_x).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    result = soup.get_text().split(',')
 
     if message.author == client.user:
         return
@@ -34,10 +41,6 @@ async def on_message(message):
         await message.channel.send(final[0])
         return
     elif user_message.lower() == '!anime_list':
-        url = 'https://animechan.vercel.app/api/available/anime'
-        html = urllib.request.urlopen(url).read()
-        soup = BeautifulSoup(html, 'html.parser')
-        result = soup.get_text().split(',')
         for a in result:
             if "[" in a:
                 b = a.split('[')
@@ -48,5 +51,32 @@ async def on_message(message):
             else:
                 await message.channel.send(a)
         return
+    elif user_message == '!jojo_stands':
+        await message.channel.send("!jojo- Random quotes from various anime."
+                                   "\n!anime_list- List of anime from which quotes are taken.")
+
+    for a in result:
+        if "[" in a:
+            b = a.split('[')
+            a = b[1]
+            if a.lower() in user_message.lower():
+                url = 'https://animechan.vercel.app/api/quotes/anime/title?=' + a;
+                html = urllib.request.urlopen(url).read()
+                soup = BeautifulSoup(html, 'html.parser')
+                print(soup[1])
+        elif "]" in a:
+            b = a.split(']')
+            a = b[0]
+            if a.lower() in user_message.lower():
+                url = 'https://animechan.vercel.app/api/quotes/anime/title?=' + a;
+                html = urllib.request.urlopen(url).read()
+                soup = BeautifulSoup(html, 'html.parser')
+                print(soup[1])
+        else:
+            if a.lower() in user_message.lower():
+                url = 'https://animechan.vercel.app/api/quotes/anime/title?=' + a;
+                html = urllib.request.urlopen(url).read()
+                soup = BeautifulSoup(html, 'html.parser')
+                print(soup[1])
 
 client.run(TOKEN)
