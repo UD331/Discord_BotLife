07ec865870b62c82eec6c1c0471cb4b473f6d6e9
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2022-05-24 19:37:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 # bot.py
 import urllib.request
-
+import random
 import discord
 from bs4 import BeautifulSoup
 
@@ -49,9 +49,11 @@ async def on_message(message):
                 await message.channel.send(a)
         return
     elif user_message.lower() == '!jojo_stands':
-        await message.channel.send("!jojo- Random quotes from various anime."
-                                   "\n!jojo_jojo- 10 random quotes from various anime."
-                                   "\n!anime_list- List of anime from which quotes are taken.")
+        await message.channel.send("!jojo: Random quotes from various anime.\n"
+                                   "!jojo_jojo: 10 random quotes from various anime.\n")
+        await message.channel.send("!jojo_(name of title with spaces in between words): " +
+                                   "Quotes from that particular anime if it is present in the database.\n"
+                                   "!anime_list: List of anime from which quotes are taken.")
     elif user_message.lower() == '!jojo_jojo':
         url = 'https://animechan.vercel.app/api/quotes'
         html = urllib.request.urlopen(url).read()
@@ -63,4 +65,23 @@ async def on_message(message):
                 print()
             else:
                 await message.channel.send(final[0])
+    elif '!jojo_' in user_message:
+        anime = user_message.split('_')
+        name = anime[1].replace(' ', '%20')
+        print(name)
+        url = 'https://animechan.vercel.app/api/quotes/anime?title=' + name
+        html = urllib.request.urlopen(url).read()
+        soup = BeautifulSoup(html, 'html.parser')
+        result = soup.get_text().split(':')
+        n = random.randrange(1, 10)
+        c = 1
+        for a in result:
+            final = a.split('}')
+            if ('anime' in final[0]) or ('character' in final[0]) or ('quote' in final[0]) or not(' ' in a):
+                print()
+            else:
+                if c == n:
+                    await message.channel.send(final[0])
+                    break
+                c += 1
 client.run(TOKEN)
